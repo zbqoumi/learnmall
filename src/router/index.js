8fea28c20a0a732bda8bd8893da140e116532975
zbqoumi <1368433931@qq.com>
2019-12-04 17:50:58 +0800

    'study'

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Home = () => import('views/home/Home')
+const Cart = () => import('views/cart/Cart')
+const Category = () => import('views/category/Category')
+const Profile = () => import('views/profile/Profile')
 
 Vue.use(VueRouter)
+//重复请求取消报错
+const [routerPush, routerReplace] = [VueRouter.prototype.push, VueRouter.prototype.replace];
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch(error => error);
+};
+VueRouter.prototype.replace = function replace(location) {
+  return routerReplace.call(this, location).catch(error => error);
+};
+
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
   }
 ]
 
@@ -25,5 +45,4 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
 export default router
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            //extensions: [], 配置省略的后缀名  已经默认配置过
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views',
+            }
+        }
+    }
+}
