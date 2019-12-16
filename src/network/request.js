e2d14eea1a39a15422bba51059cfdd3b9e237ab8
zbqoumi <1368433931@qq.com>
2019-12-16 17:29:18 +0800

    '更新项目'

---
+++ src/network/request.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export function request(config) {
+  const instance = axios.create({
+    baseURL: 'http://106.54.54.237:8000/api/v1'
+  })
+
+  return instance(config)
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,41 @@
 <template>
-    <h2>主页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from "components/common/navbar/NavBar"
+  import HomeSwiper from "./childComps/HomeSwiper"
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    data() {
+      return{
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+      })
+    }
     }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
